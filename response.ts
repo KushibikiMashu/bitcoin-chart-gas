bf3d4589c3e5c76fc58615e61cf441600648fe12
KushibikiMashu <kushibiki3413@gmail.com>
2019-05-01 00:53:59 +0900

    微修正

--- response.ts
+++ response.ts
@@ -1,6 +1,6 @@
 // controllerの責務がある
 function doGet() {
     const data = (new BitcoinChartModel()).getAllSheetData();
-    return ContentService.createTextOutput(JSON.stringify(data, null, 2))
+    return ContentService.createTextOutput(JSON.stringify(data))
         .setMimeType(ContentService.MimeType.JSON);
 }
